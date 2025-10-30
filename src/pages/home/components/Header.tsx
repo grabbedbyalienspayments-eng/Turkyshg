53165c43b70386bff098ec322e48b9519694cdc2
grabbedbyalienspayments-eng <grabbedbyalienspayments@gmail.com>
2025-10-30 17:05:13 +0200

    Replace logo SVG with image for header

--- src/pages/home/components/Header.tsx
+++ src/pages/home/components/Header.tsx
@@ -20,34 +20,11 @@ export default function Header() {
         <div className="flex items-center justify-between h-20">
           {/* Logo */}
           <div className="flex items-center">
-            <div className="h-16 w-16 bg-white rounded-lg flex items-center justify-center shadow-sm p-2">
-              {/* Semiluna și steaua turcească */}
-              <svg viewBox="0 0 100 100" className="w-full h-full">
-                {/* Semiluna roșie */}
-                <path
-                  d="M 30 50 A 20 20 0 1 1 30 50 A 16 16 0 1 0 30 50 Z"
-                  fill="#C8102E"
-                  transform="translate(15, 0)"
-                />
-                {/* Steaua roșie */}
-                <polygon
-                  points="65,35 68,44 77,44 70,49 73,58 65,53 57,58 60,49 53,44 62,44"
-                  fill="#C8102E"
-                />
-              </svg>
-            </div>
-            <div className="ml-3">
-              <div className={`text-lg font-bold transition-colors ${
-                isScrolled ? 'text-[#C8102E]' : 'text-white'
-              }`} style={{ fontFamily: 'Playfair Display, serif' }}>
-                Saray Kebap
-              </div>
-              <div className={`text-xs -mt-1 transition-colors ${
-                isScrolled ? 'text-[#C8102E]/70' : 'text-white/80'
-              }`}>
-                Cluj-Napoca
-              </div>
-            </div>
+            <img 
+              src="/turkish-grill-logo.png" 
+              alt="Saray Kebap Cluj - Turkish Grill" 
+              className="h-16 w-auto object-contain"
+            />
           </div>
 
           {/* CTA Button */}
